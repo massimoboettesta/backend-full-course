50a864b718114f59fecef5cb9d4837382c6ad329
massimoboettesta <max_wapo97@hotmail.com>
2025-04-07 21:48:17 +0200

    added endpoint types

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -1,11 +1,18 @@
+//Address
+//URL -> http://localhost:8383
 const express = require('express')
 const app = express()
 const PORT = 8383
 
-
-app.listen(PORT,() => console.log(`Server has started on port on: ${PORT}`))
-
-app.get('/',(req, res) =>{
-    res.send('Hello world!')
+//Type 1 - Window (visual)
+app.get('/', (req, res) => {
+    res.send('<h1>homepage</h1>')
 })
 
+app.get('/dashboard', (req, res) => {
+    res.send('<h1>dashboard</h1>')
+})
+
+//Type 2 - API (non visual)
+
+app.listen(PORT,() => console.log(`Server has started on port on: ${PORT}`))
