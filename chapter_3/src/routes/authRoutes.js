0781106776495d441a2f0bc608fc34931d7f7e00
massimoboettesta <max_wapo97@hotmail.com>
2025-04-09 13:34:40 +0200

    added auth register

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -5,8 +5,30 @@ import db from '../db.js'
 
 const router = express.Router()
 
+// Register new user endpoint /auth/register
 router.post('/register', (req, res) => {
+    const {username, password} = req.body
+    // Encrypt the password 
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    // Save the new user and hashedPassword to the db
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password)
+            VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+        
+        // now that we have a user, We're going to add their first todo
+        const defaultTodo = `Helo :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
+            VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid,defaultTodo)
 
+        // create a token
+        const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'})
+        res.json({token})
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 })
 
 router.post('/login', (req, res) => {
--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5003
@@ -24,6 +25,7 @@ app.get('/', (req, res)=> {
 
 //Routes
 app.use('/auth', authRoutes)
+app.use('/todo', todoRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server has started on port:${PORT}`)
